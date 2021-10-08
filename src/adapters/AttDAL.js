5ab124eebd82d8ae0cc3809aa51c09c0e274402f
titanium25 <idpm7N95tLPLskg>
2021-10-08 21:19:33 +0300

    add heroku API

--- src/adapters/AttDAL.js
+++ src/adapters/AttDAL.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://13.51.205.210:2020/api/att';
+const url = 'https://att-api-alex.herokuapp.com/api/att';
 
 const getAtt = () => {
     return axios.get(url)
